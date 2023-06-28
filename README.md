fc5a945b1a44443304e6fada085d07bd72674526
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-06-28 11:41:08 -0500

    Updated readme file and SavedBooks.js

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # book-search-engine
+  
+![badge](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+This application takes an existing book search react app and adds functionality for it to work with GraphQL and Apollo Server. This application is deployed through Heroku.
+
+## Table of Contents
+- [Description](#description)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Deployed Link](#deployed-link)
+- [Demo](#demo)
+- [Built With](#built-with)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+`git clone` the repo to your local machine. To use this application, run the following command to install the dependencies: 
+
+     npm install
+
+Then run the following command:
+
+`npm run start:dev`
+
+## Usage
+Type the following command in your termimal:
+
+`npm run start`
+
+Then open your browser and go to http://localhost:3000/ to run this application on your local machine.
+
+## Deployed Link
+
+The link to the deployed heroku application is: https://book-search-engine-scastillo-d8d9f72cad16.herokuapp.com/.
+
+The link to the deployed application is: https://github.com/stephen-castillo/book-search-engine.
+
+
+## Demo
+Below is the demostration of this application. 
+
+
+**[⬆ back to top](#table-of-contents)**
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/Javascript)
+* [NodeJS](https://nodejs.org/en/)
+* [ExpressJS](https://expressjs.com/)
+* [React](https://react.dev/)
+* [react-bootstrap](https://www.npmjs.com/package/react-bootstrap)
+* [GraphQL](https://graphql.org/)
+* [Apollo-Server](https://www.apollographql.com/)
+  
+**[⬆ back to top](#table-of-contents)**
+
+## License
+This application is licensed under MIT license. 
+
+**[⬆ back to top](#table-of-contents)**
+
+## Contributing
+This application is developed by Stephen Castillo. If you would like to contribute it, please follow the guidelines for how to do so.
+
+**[⬆ back to top](#table-of-contents)**
+
+## Tests
+There are no tests for this program at this time.
+
+
+**[⬆ back to top](#table-of-contents)**
+
+## Questions
+For any question, please contact me on GitHub: [stephen-castillo](https://github.com/stephen-castillo) or email me at email@sampleemail.com.
+
+**[⬆ back to top](#table-of-contents)**
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,46 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import {
   Container,
   Card,
   Button,
   Row,
   Col
-} from 'react-bootstrap';
+} from "react-bootstrap";
 
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from "@apollo/client";
+import { REMOVE_BOOK } from "../utils/mutations";
+import { GET_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  
+    const { loading, data } = useQuery(GET_ME);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+    const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+    const userData = data?.me || {};
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -50,15 +29,15 @@ const SavedBooks = () => {
       return false;
     }
 
+    const decodedToken = Auth.getProfile();
+    console.log("decoded Token!!-->>>", decodedToken);
+    
+    const usersId = decodedToken.data._id;
+
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
+      const response = await removeBook({
+        variables: { userId: usersId, bookId },
+      });
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -66,35 +45,35 @@ const SavedBooks = () => {
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
+  // if data isn"t here yet, say so
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <div fluid className="text-light bg-dark p-5">
+      <div fluid="true" className="text-light bg-dark p-5">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
       </div>
       <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+        <h2 className="pt-5">
+          {userData.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? "book" : "books"}:`
+            : "You have no saved books!"}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Col md="4" key={book.bookId}>
+                <Card key={book.bookId} border="dark">
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant="top" /> : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
+                    <p className="small">Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button className="btn-block btn-danger" onClick={() => handleDeleteBook(book.bookId)}>
                       Delete this Book!
                     </Button>
                   </Card.Body>
